aec6853d27afe38b9ea708a7a8f6dbd3cb2d477d
Olivier Lange <olange@petit-atelier.ch>
2014-11-27 22:44:42 +0100

    Ajusté code détecteur pour qu'il corresponde au schéma câblage

--- code/detecteur-presence/Ultrasonic-HC-SR04/Ultrasonic-HC-SR04.ino
+++ code/detecteur-presence/Ultrasonic-HC-SR04/Ultrasonic-HC-SR04.ino
@@ -1,7 +1,19 @@
+/*
+  Script Arduino pour lire la distance livree par le
+  detecteur de presence sur le port serie.
+  
+  Voir illustration arduino-uno-ultrasonic-hc-sr04-wiring.png
+  pour le schema de cablage.
+
+  Alternativement, raccorder ECHO sur le pin 13 pour avoir un
+  feedback sur la LED du board Arduino; ajuster la constante ECHO
+  a la valeur 13 le cas echeant.
+*/
+
 #include <Ultrasonic.h>
 
 #define TRIGGER_PIN  12
-#define ECHO_PIN     13
+#define ECHO_PIN     11
 
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 
@@ -17,7 +29,7 @@ void loop()
 
   cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
 
-  //centimetres
+  // Centimetres
   Serial.println(cmMsec);
 
   delay(100);
